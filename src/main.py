265774fbce0be21319527c9064ed03d354dd1b82
Vedant2116 <vedqntgohel@gmail.com>
2024-04-14 15:58:47 -0400

    Commit

---
+++ src/main.py
@@ -0,0 +1,41 @@
+import os
+
+import cvzone
+import cv2
+from cvzone.PoseModule import PoseDetector
+
+cap = cv2.VideoCapture(0)
+detector = PoseDetector()
+
+shirtFolderPath = "Resources/Shirts"
+listShirts = os.listdir(shirtFolderPath)
+# print(listShirts)
+fixedRatio = 262 / 190  # widthOfShirt/widthOfPoint11to12
+shirtRatioHeightWidth = 581 / 440
+imageNumber = 0
+
+while True:
+    success, img = cap.read()
+    img = detector.findPose(img)
+    # img = cv2.flip(img,1)
+    lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False, draw=False)
+    if lmList:
+        # center = bboxInfo["center"]
+        lm11 = lmList[11][1:3]
+        lm12 = lmList[12][1:3]
+        imgShirt = cv2.imread(os.path.join(shirtFolderPath, listShirts[0]), cv2.IMREAD_UNCHANGED)
+
+        widthOfShirt = int((lm11[0] - lm12[0]) * fixedRatio)
+        print(widthOfShirt)
+        imgShirt = cv2.resize(imgShirt, (widthOfShirt, int(widthOfShirt * shirtRatioHeightWidth)))
+        currentScale = (lm11[0] - lm12[0]) / 190
+        offset = int(44 * currentScale), int(48 * currentScale)
+
+        try:
+            img = cvzone.overlayPNG(img, imgShirt, (lm12[0] - offset[0], lm12[1] - offset[1]))
+        except:
+            pass
+
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
